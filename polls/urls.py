175451ecb0b7bad3cb80fa766219305877623677
Gourav Singh Rawat <69115613+Seek4samurai@users.noreply.github.com>
2021-10-05 19:04:34 +0530

    added http404 request on Question_ID not found

---
+++ polls/urls.py
@@ -0,0 +1,14 @@
+from django.urls import path
+from django.urls.conf import include
+from . import views
+
+urlpatterns = [
+    # ex: /polls
+    path('', views.index, name='index'),
+    # ex: /polls/10
+    path('<int:question_id>/', views.details, name='details'),
+    # ex: /polls/10/results
+    path('<int:question_id>/results/', views.results, name='results'),
+    # ex: /polls/10/vote
+    path('<int:question_id>/vote/', views.vote, name='vote'),
+]
