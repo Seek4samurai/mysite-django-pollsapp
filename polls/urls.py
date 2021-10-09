8cea82f2dc9bf5d28049eb62d86a9b9114f2a80c
Gourav Singh Rawat <69115613+Seek4samurai@users.noreply.github.com>
2021-10-09 16:08:50 +0530

    working polls
    choice.vote not working yet...

--- polls/urls.py
+++ polls/urls.py
@@ -2,6 +2,7 @@ from django.urls import path
 from django.urls.conf import include
 from . import views
 
+app_name = "polls"
 urlpatterns = [
     # ex: /polls
     path('', views.index, name='index'),
